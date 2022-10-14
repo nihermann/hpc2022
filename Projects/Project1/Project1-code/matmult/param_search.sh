c14a4cbeb46232d6ccf19450cd5eb586e612d316
nihermann <hermann.nicolai@googlemail.com>
2022-10-14 21:14:08 +0200

    Included param_search shell script and added likwid-topology to both scripts to make sure the runs are comparable

--- Projects/Project1/Project1-code/matmult/param_search.sh
+++ Projects/Project1/Project1-code/matmult/param_search.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=matrixmult
-#SBATCH --time=02:00:00
+#SBATCH --job-name=param_search
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --output=matrixmult-%j.out
-#SBATCH --error=matrixmult-%j.err
+#SBATCH --output=param_search-%j.out
+#SBATCH --error=param_search-%j.err
 
 # load modules
 if command -v module 1>/dev/null 2>&1; then
-   module load gcc/10.1.0 intel-mkl/2020.1.217-gcc-10.1.0-qsctnr6 gnuplot
+   module load gcc/10.1.0 intel-mkl/2020.1.217-gcc-10.1.0-qsctnr6 gnuplot likewid
 fi
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
-
+likwid-topology
 echo "==== benchmark-blocked block size search ===================="
 ./benchmark-blocked | tee timing_blocked_dgemm.data
--- Projects/Project1/Project1-code/matmult/run_matrixmult.sh
+++ Projects/Project1/Project1-code/matmult/run_matrixmult.sh
@@ -8,12 +8,12 @@
 
 # load modules
 if command -v module 1>/dev/null 2>&1; then
-   module load gcc/10.1.0 intel-mkl/2020.1.217-gcc-10.1.0-qsctnr6 gnuplot
+   module load gcc/10.1.0 intel-mkl/2020.1.217-gcc-10.1.0-qsctnr6 gnuplot likwid
 fi
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
-
+likwid-topology
 echo "==== benchmark-naive ======================"
 ./benchmark-naive | tee timing_basic_dgemm.data
 echo
