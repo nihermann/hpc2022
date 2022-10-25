79ba8ba8ccefcd374348562ee7155ff201f34e56
nihermann <hermann.nicolai@googlemail.com>
2022-10-25 14:42:01 +0200

    Indented lines

--- Projects/Project2/Project2-code/hist/hist_omp.cpp
+++ Projects/Project2/Project2-code/hist/hist_omp.cpp
@@ -8,49 +8,49 @@
 using namespace std;
 
 int main() {
-  double time_start, time_end;
+    double time_start, time_end;
 
-  // Initialize random number generator
-  unsigned int seed = 123;
-  float mean = BINS / 2.0;
-  float sigma = BINS / 12.0;
-  std::default_random_engine generator(seed);
-  std::normal_distribution<float> distribution(mean, sigma);
+    // Initialize random number generator
+    unsigned int seed = 123;
+    float mean = BINS / 2.0;
+    float sigma = BINS / 12.0;
+    std::default_random_engine generator(seed);
+    std::normal_distribution<float> distribution(mean, sigma);
 
-  // Generate random sequence
-  // Note: normal distribution is on interval [-inf; inf]
-  //       we want [0; BINS-1]
-  int *vec = new int[VEC_SIZE];
-  for (long i = 0; i < VEC_SIZE; ++i) {
-    vec[i] = int(distribution(generator));
-    if (vec[i] < 0)
-      vec[i] = 0;
-    if (vec[i] > BINS - 1)
-      vec[i] = BINS - 1;
-  }
+    // Generate random sequence
+    // Note: normal distribution is on interval [-inf; inf]
+    //       we want [0; BINS-1]
+    int *vec = new int[VEC_SIZE];
+    for (long i = 0; i < VEC_SIZE; ++i) {
+        vec[i] = int(distribution(generator));
+        if (vec[i] < 0)
+            vec[i] = 0;
+        if (vec[i] > BINS - 1)
+            vec[i] = BINS - 1;
+    }
 
-  // Initialize histogram
-  // Set all bins to zero
-  long dist[BINS];
-  for (int i = 0; i < BINS; ++i) {
-    dist[i] = 0;
-  }
+    // Initialize histogram
+    // Set all bins to zero
+    long dist[BINS];
+    for (int i = 0; i < BINS; ++i) {
+        dist[i] = 0;
+    }
 
-  time_start = wall_time();
+    time_start = wall_time();
 
-  // TODO Parallelize the histogram computation
-  for (long i = 0; i < VEC_SIZE; ++i) {
-    dist[vec[i]]++;
-  }
-  time_end = wall_time();
+    // TODO Parallelize the histogram computation
+    for (long i = 0; i < VEC_SIZE; ++i) {
+        dist[vec[i]]++;
+    }
+    time_end = wall_time();
 
-  // Write results
-  for (int i = 0; i < BINS; ++i) {
-    cout << "dist[" << i << "]=" << dist[i] << endl;
-  }
-  cout << "Time: " << time_end - time_start << " sec" << endl;
+    // Write results
+    for (int i = 0; i < BINS; ++i) {
+        cout << "dist[" << i << "]=" << dist[i] << endl;
+    }
+    cout << "Time: " << time_end - time_start << " sec" << endl;
 
-  delete[] vec;
+    delete[] vec;
 
-  return 0;
+    return 0;
 }
