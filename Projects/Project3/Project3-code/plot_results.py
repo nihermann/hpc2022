4eac25a0cc2d1e1b34f687296b3d7070558b0fb5
nihermann <hermann.nicolai@googlemail.com>
2022-11-09 16:03:06 +0100

    Changed scatter to line plot for strong scaling

--- Projects/Project3/Project3-code/plot_results.py
+++ Projects/Project3/Project3-code/plot_results.py
@@ -4,7 +4,7 @@ import pandas as pd
 def strong():
     df = pd.read_csv("slurm-1441825.out")
     df = df.astype({"Grid Size": "category"})
-    plot = ggplot(df, aes(x="Threads", y="Time", color="Grid Size")) + facet_wrap("Grid Size", nrow=2, scales="free") + geom_point() + theme(subplots_adjust={'wspace': 0.25, 'hspace': .25})
+    plot = ggplot(df, aes(x="Threads", y="Time", color="Grid Size")) + facet_wrap("Grid Size", nrow=2, scales="free") + geom_line() + theme(subplots_adjust={'wspace': 0.25, 'hspace': .25})
     plot.save("strong_scaling.png")
 
 
